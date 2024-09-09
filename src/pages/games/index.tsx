41589402fb5c1243e5bbedd9501bb427d09d50fb
NorVirae <norbertmbafrank@gmail.com>
2024-09-09 18:52:16 +0100

    fix BLACK-234 task

--- src/pages/games/index.tsx
+++ src/pages/games/index.tsx
@@ -6,16 +6,19 @@ import TabSection1 from "@/components/section/TabSection1";
 
 import MetaComponent from "@/components/common/MetaComponent";
 import GameListing from "@/components/section/GameListing";
+import { useRouter } from "next/router";
 const metadata = {
   title: "Blackhards - Game Marketplace | Games",
 };
 
 export default function Games() {
+
+  const router = useRouter()
   return (
     <MarketLayouts>
       <MetaComponent meta={metadata} />
       <TabSection1 />
-      <Breadcumb3 path={["Home", "Auctions", "Design & Creative"]} />
+      <Breadcumb3 path={["Home", router.pathname.split("/")[1]]} />
       <Breadcumb4 />
       <GameListing />
     </MarketLayouts>
