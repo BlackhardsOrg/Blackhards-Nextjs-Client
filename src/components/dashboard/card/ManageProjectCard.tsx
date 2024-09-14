afe45217d03e8954dbf1fb1979f1704dc2a55538
NorVirae <norbertmbafrank@gmail.com>
2024-09-14 22:37:35 +0100

    updated to frontend auth

--- src/components/dashboard/card/ManageProjectCard.tsx
+++ src/components/dashboard/card/ManageProjectCard.tsx
@@ -3,6 +3,7 @@ import { IGameTitleGQL } from "@/types";
 import { timeAgo } from "@/utils";
 import { formatPriceToDollars } from "@/utils/priceFormatter";
 import { useQuery } from "@apollo/client";
+import Link from "next/link";
 import { useEffect } from "react";
 import { Tooltip } from "react-tooltip";
 
@@ -15,7 +16,7 @@ export default function ManageProjectCard({gametitle}:{gametitle?: IGameTitleGQL
           <div className="freelancer-style1 box-shadow-none row m-0 p-0 align-items-lg-end">
             <div className="d-lg-flex px-0">
               <div className="details mb15-md-md">
-                <h5 className="title mb10">{gametitle?.title}</h5>
+                <h5 className="title mb10"><Link href={"/games/game-preview/" + gametitle?._id}>{gametitle?.title}</Link> </h5>
                 <p className="mb-0 fz14 list-inline-item mb5-sm pe-1">
                   <i className="flaticon-video-file fz16 vam text-thm2 me-1" />{" "}
                   {gametitle?.gamePlays} plays
