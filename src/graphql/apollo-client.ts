b8cadb7e94d873846d2bf91fe79aef448f776509
NorVirae <norbertmbafrank@gmail.com>
2024-10-03 10:14:18 +0100

    add url to graphql

--- src/graphql/apollo-client.ts
+++ src/graphql/apollo-client.ts
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const createApolloClient = () => {
   return new ApolloClient({
-    uri: "http://localhost:8080/graphql",
+    uri: process.env.NEXT_PUBLIC_API_URL+"/graphql",
     cache: new InMemoryCache(),
   });
 };
